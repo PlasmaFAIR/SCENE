8bec4e1918df77881dc9e8991fd40c9043d0a953
Peter Hill <zed.three@gmail.com>
2021-05-13 17:59:04 +0100

    Build and run tests on CI

--- .ci_scripts/ci_build.sh
+++ .ci_scripts/ci_build.sh
@@ -5,6 +5,9 @@ cmake . -B build \
       -DGHOST_ROOT=${GHOST_INSTALL_DIR} \
       -DGHOST_DEBUG=ON \
       -DnetCDFFortran_DEBUG=ON \
-      -DnetCDF_Fortran_INCLUDE_DIR=/usr/lib64/gfortran/modules/
+      -DnetCDF_Fortran_INCLUDE_DIR=/usr/lib64/gfortran/modules/ \
+      -DBUILD_TESTING=ON
 
 cmake --build build
+
+cmake --build build --target check
