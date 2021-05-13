f0b20b398f4fa9c223ddb3e3ecd51c5a2584de9f
Peter Hill <zed.three@gmail.com>
2021-05-13 15:22:19 +0100

    Try setting netcdf include dir directly

--- .ci_scripts/ci_build.sh
+++ .ci_scripts/ci_build.sh
@@ -3,6 +3,8 @@ set -ex
 cmake --version
 cmake . -B build \
       -DGHOST_ROOT=${GHOST_INSTALL_DIR} \
-      -DGHOST_DEBUG=ON
+      -DGHOST_DEBUG=ON \
+      -DnetCDFFortran_DEBUG=ON \
+      -DnetCDF_Fortran_INCLUDE_DIR=/usr/lib64/gfortran/modules/
 
 cmake --build build
