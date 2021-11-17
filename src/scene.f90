109f1d426bd3c60c4078b44bab948be7db61a164
Peter Hill <peter.hill@york.ac.uk>
2021-11-17 12:07:15 +0000

    Add GPLv3 licence

--- src/scene.f90
+++ src/scene.f90
@@ -1,3 +1,20 @@
+! SCENE: Tokamak magnetic equilibrium solver
+! Copyright (C) 2021 Howard Wilson, Bhavin Patel, David Dickinson, Peter Hill
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+!> SPDX-License-Identifier: GPL-3.0-only
 program scene
   use bootstap, only : fastbs
   use equilibrium, only : equil, xarea
